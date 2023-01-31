7a0795bd583f58d1201d723c901eb19c8546bfb6
Steven Gettys <s.gettys@f5.com>
2023-01-31 13:32:02 -0800

    chore: Updated grpc integration tests to inject installations into the porter test client
    Signed-off-by: Steven Gettys <s.gettys@f5.com>

--- tests/grpc/install_test.go
+++ tests/grpc/install_test.go
@@ -7,6 +7,7 @@ import (
 
 	iGRPC "get.porter.sh/porter/gen/proto/go/porterapis/installation/v1alpha1"
 	pGRPC "get.porter.sh/porter/gen/proto/go/porterapis/porter/v1alpha1"
+	"get.porter.sh/porter/pkg/storage"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
@@ -15,6 +16,8 @@ import (
 func TestInstall_installationMessage(t *testing.T) {
 	t.Parallel()
 	grpcSvr, _ := NewTestGRPCServer(t)
+	i1 := storage.NewInstallation("", "test")
+	grpcSvr.TestPorter.TestInstallations.CreateInstallation(i1)
 	ctx := grpcSvr.TestPorter.SetupIntegrationTest()
 	grpcSvr.TestPorter.AddTestBundleDir("../integration/testdata/bundles/bundle-with-custom-action", true)
 	grpcSvr.ListenAndServe()
@@ -27,5 +30,7 @@ func TestInstall_installationMessage(t *testing.T) {
 	instClient := pGRPC.NewPorterBundleClient(conn)
 	resp, err := instClient.ListInstallations(context.Background(), &iGRPC.ListInstallationsRequest{})
 	require.NoError(t, err)
-	assert.Equal(t, `installation:{name:"test installation" namespace:"foo" bundle:{repository:"test.repo" version:"v1.0.0"}}`, resp.String())
+	//assert.Equal(t, `installation:{name:"test installation" namespace:"foo" bundle:{repository:"test.repo" version:"v1.0.0"}}`, resp.String())
+	assert.Len(t, resp.Installation, 1)
+	assert.Equal(t, resp.Installation[0].Name, "test")
 }
