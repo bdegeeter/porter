7991d34e75e5874b1e31112404127ba0353c553d
Steven Gettys <s.gettys@f5.com>
2023-01-30 16:52:14 -0800

    chore: Added UT for grpc context package
    Signed-off-by: Steven Gettys <s.gettys@f5.com>

---
+++ pkg/grpc/context/context_test.go
@@ -0,0 +1,55 @@
+package context
+
+import (
+	"context"
+	"testing"
+
+	"get.porter.sh/porter/pkg/porter"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+)
+
+func TestGetPorterConnectionFromContextReturnsErrIfNoConnectionInContext(t *testing.T) {
+	ctx := context.Background()
+	p, err := GetPorterConnectionFromContext(ctx)
+	assert.Nil(t, p)
+	assert.EqualError(t, err, "Unable to find porter connection in context")
+}
+
+func TestGetPorterConnectionFromContextReturnsPorterConnection(t *testing.T) {
+	p := porter.New()
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, porterConnCtxKey, p)
+	newP, err := GetPorterConnectionFromContext(ctx)
+	assert.Nil(t, err)
+	assert.Equal(t, p, newP)
+}
+func TestAddPorterConnectionToContextReturnsContextUpdatedWithPorterConnection(t *testing.T) {
+	p := porter.New()
+	ctx := context.Background()
+	ctx = AddPorterConnectionToContext(p, ctx)
+	newP, ok := ctx.Value(porterConnCtxKey).(*porter.Porter)
+	assert.True(t, ok)
+	assert.Equal(t, p, newP)
+}
+
+// Have to figure out how to test
+func TestNewConnectionInterceptorReturnsErrorIfUnableToSetupPorterConnection(t *testing.T) {
+
+}
+
+func TestNewConnectionInterceptorCallsNextHandlerInTheChainWithThePorterConnectionInTheContext(t *testing.T) {
+	parentUnaryInfo := &grpc.UnaryServerInfo{FullMethod: "SomeService.StreamMethod"}
+	input := "input"
+	testHandler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		p, err := GetPorterConnectionFromContext(ctx)
+		return p, err
+	}
+	ctx := context.Background()
+	chain := grpc_middleware.ChainUnaryServer(NewConnectionInterceptor)
+	newP, err := chain(ctx, input, parentUnaryInfo, testHandler)
+	assert.Nil(t, err)
+	assert.NotNil(t, newP)
+	assert.IsType(t, &porter.Porter{}, newP)
+}
---
+++ pkg/grpc/installation/installation_test.go
@@ -0,0 +1,15 @@
+package installation
+
+import "testing"
+
+func TestListInstallationReturnsListOfPorterInstallations(t *testing.T) {
+
+}
+
+func TestListInstallationsReturnsErrorIfUnableToGetPorterConnectionFromRequestContext(t *testing.T) {
+}
+
+// This may not be reachable the way the test client is currently setup
+func TestListInstallationsReturnsErrorIfUnableToListPorterInstallations(t *testing.T) {
+
+}
